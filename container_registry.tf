b4872a7c37dd2cd2331a0d68d1a9deb8caf18d2e
Benjamin Brewer <bbrewer@pillartechnology.com>
2018-07-30 11:44:22 -0400

    SMRT-302 add repository for terraform jenkins worker
    co-authored-by: Jeff Grunewald <jeff@grunewalddesign.com>

--- container_registry.tf
+++ container_registry.tf
@@ -45,3 +45,10 @@ module "cota_streaming_consumer_repository" {
   alm_account_id  = "${var.alm_account_id}"
   repository_name = "cota-streaming-consumer"
 }
+
+module "jenkins_worker_terraform_repository" {
+  source = "../modules/ecr"
+
+  alm_account_id  = "${var.alm_account_id}"
+  repository_name = "jenkins-worker-terraform"
+}
