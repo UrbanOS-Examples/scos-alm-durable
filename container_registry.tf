8b78bf86422ba1f426976b58bca83f04c97cfddc
paul-jacob <plinville@pillartechnology.com>
2018-08-16 16:29:12 -0400

    SMRT-259 Imported ECR repos that were not in Terraform

--- container_registry.tf
+++ container_registry.tf
@@ -33,22 +33,49 @@ module "cota_streaming_ui_smoke_test_repository" {
 }
 
 module "leaflet_notebook_repository" {
-  source = "../modules/ecr"
-
+  source          = "../modules/ecr"
   alm_account_id  = "${var.alm_account_id}"
   repository_name = "leaflet-notebook"
 }
 
 module "cota_streaming_consumer_repository" {
-  source = "../modules/ecr"
-
+  source          = "../modules/ecr"
   alm_account_id  = "${var.alm_account_id}"
   repository_name = "cota-streaming-consumer"
 }
 
 module "jenkins_worker_infrastructure_repository" {
-  source = "../modules/ecr"
-
+  source          = "../modules/ecr"
   alm_account_id  = "${var.alm_account_id}"
   repository_name = "jenkins-worker-infrastructure"
+}
+
+module "cota_streaming_producer_repository" {
+  source          = "../modules/ecr"
+  alm_account_id  = "${var.alm_account_id}"
+  repository_name = "cota-streaming-producer"
+}
+
+module "cota_streaming_ui_repository" {
+  source          = "../modules/ecr"
+  alm_account_id  = "${var.alm_account_id}"
+  repository_name = "cota-streaming-ui"
+}
+
+module "jenkins_master_repository" {
+  source          = "../modules/ecr"
+  alm_account_id  = "${var.alm_account_id}"
+  repository_name = "jenkins-master"
+}
+
+module "jenkins_relay_repository" {
+  source          = "../modules/ecr"
+  alm_account_id  = "${var.alm_account_id}"
+  repository_name = "jenkins-relay"
+}
+
+module "jupyterhub_repository" {
+  source          = "../modules/ecr"
+  alm_account_id  = "${var.alm_account_id}"
+  repository_name = "jupyterhub"
 }
