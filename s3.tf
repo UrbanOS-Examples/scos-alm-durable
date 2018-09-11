aa35b0909430b7e7068c123ed7139ba3c89ffcc6
Brandon Cromer <bcromer@pillartechnology.com>
2018-09-11 16:38:18 -0400

    SMRT-454 Made the build artifacts public, installed the ckan theme from s3

--- s3.tf
+++ s3.tf
@@ -63,13 +63,29 @@ POLICY
 
 resource "aws_s3_bucket" "build_artifacts_repository" {
   bucket = "${var.build_artifacts_repo_name}"
-  acl    = "private"
+  acl    = "public-read"
   tags {
     Name        = "build-artifacts-repository"
     Environment = "${terraform.workspace}"
   }
 }
 
+resource "aws_s3_bucket_policy" "build_artifacts_repository_policy" {
+  bucket = "${aws_s3_bucket.build_artifacts_repository.id}"
+  policy =<<POLICY
+{
+  "Version":"2012-10-17",
+  "Statement":[{
+      "Sid":"AddPerm",
+      "Effect":"Allow",
+      "Principal": "*",
+      "Action":["s3:GetObject"],
+      "Resource":["${aws_s3_bucket.build_artifacts_repository.arn}/*"]
+    }
+  ]
+}
+POLICY
+}
 variable "build_artifacts_repo_name" {
   default = "os-build-artifacts-repository"
   description = "Bucket name to archive build artifacts"
