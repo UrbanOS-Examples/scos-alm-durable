d5cea06a3de90955b46e7197070ec726fbed5745
jmorris-pillar <jmorris2@pillartechnology.com>
2018-10-01 13:47:16 -0400

    Added a new repository for kylo api (#237)

--- container_registry.tf
+++ container_registry.tf
@@ -85,6 +85,11 @@ module "micro_service_watchinator_repository" {
   alm_account_id  = "${var.alm_account_id}"
   repository_name = "micro-service-watchinator"
 }
+module "discovery_api_repository" {
+  source          = "../modules/ecr"
+  alm_account_id  = "${var.alm_account_id}"
+  repository_name = "discovery-api"
+}
 
 module "discovery_ui_repository" {
   source          = "../modules/ecr"
